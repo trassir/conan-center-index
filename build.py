abb463da7f6707c143e319567a22bf239a72c5e3
worldemar <woldemar@mimas.ru>
2020-03-05 16:57:48 +0300

    cpt username & channel workaround

--- build.py
+++ build.py
@@ -1,15 +1,28 @@
 from os import environ
+from sys import platform
 from cpt.packager import ConanMultiPackager
 
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(
-        login_username="trassir-ci-bot",
-        upload="https://api.bintray.com/conan/trassir/conan-public",
-        upload_only_when_stable=1,
-        stable_branch_pattern="trassir-ci",
-        stable_channel="_",
-        remotes="https://api.bintray.com/conan/trassir/conan-public"
-    )
+    if platform == "darwin":
+        builder = ConanMultiPackager(
+            login_username="trassir-ci-bot",
+            upload="https://api.bintray.com/conan/trassir/conan-public",
+            upload_only_when_stable=1,
+            stable_branch_pattern="cpt-username-channel-behavior",
+            stable_channel="_",
+            stable_username="_",
+            remotes="https://api.bintray.com/conan/trassir/conan-public"
+        )
+    else:
+        builder = ConanMultiPackager(
+            login_username="trassir-ci-bot",
+            upload="https://api.bintray.com/conan/trassir/conan-public",
+            upload_only_when_stable=1,
+            stable_branch_pattern="cpt-username-channel-behavior",
+            stable_channel="_",
+            stable_username="_",
+            remotes="https://api.bintray.com/conan/trassir/conan-public"
+        )
     builder.add_common_builds(pure_c=False)
     builder.run()
