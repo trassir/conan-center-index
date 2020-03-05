464e6ecbeeecbbac2163534b2a3d9e64ca9c6223
worldemar <woldemar@mimas.ru>
2020-03-05 17:45:22 +0300

    Update build.py

--- build.py
+++ build.py
@@ -22,8 +22,6 @@ if __name__ == "__main__":
             upload_only_when_stable=1,
             stable_branch_pattern="cpt-username-channel-behavior",
             stable_channel="_",
-            channel="_",
-            username="_",
             remotes="https://api.bintray.com/conan/trassir/conan-public"
         )
     builder.add_common_builds(pure_c=False)
