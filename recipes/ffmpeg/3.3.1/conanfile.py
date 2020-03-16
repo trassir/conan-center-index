568808ae3741907d13887b2791626a946eda52d5
Vladimir Looze <woldemar@mimas.ru>
2020-03-16 13:05:53 +0300

    Update conanfile.py
    deps search

--- recipes/ffmpeg/3.3.1/conanfile.py
+++ recipes/ffmpeg/3.3.1/conanfile.py
@@ -135,7 +135,8 @@ class FFMpegConan(ConanFile):
         if self.options.bzlib:
             self.requires.add("bzip2/1.0.6")
         if self.options.lzma:
-            self.requires.add("lzma/5.2.3")
+            # disable to discover more deps
+            # self.requires.add("lzma/5.2.3")
         if self.options.iconv:
             self.requires.add("libiconv/1.15")
         if self.options.freetype:
