2a802e36740937c23dfc2e3e9665b1449c449a6e
borune-k12 <borune.k12@gmail.com>
2020-04-01 20:46:12 +0300

    Update manage.py

--- recipes/x11/all/manage.py
+++ recipes/x11/all/manage.py
@@ -13,7 +13,7 @@ import re
 conanfile_template = """from conans import tools
 import os
 import sys
-print('curent dir: %s' % os.path.abspath(os.path.dirname(__file__)))
+print('curent dir: %s' % os.path.dirname(os.path.realpath(__file__)))
 print('parent dir: %s' % os.path.join(os.path.abspath(os.path.dirname(__file__)), '..'))
 print('cwd: %s' %  os.getcwd())
 sys.path.insert(0, os.path.join(os.path.abspath(os.path.dirname(__file__)), '..'))
