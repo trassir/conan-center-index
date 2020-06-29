e4bb17d019a829a7345dca0bf884a33a365ceeee
borune-k12 <borune.k12@gmail.com>
2020-06-29 15:51:26 +0300

    change meson version

--- recipes/glib/all/conanfile.py
+++ recipes/glib/all/conanfile.py
@@ -49,9 +49,7 @@ class GLibConan(ConanFile):
             del self.options.with_selinux
 
     def build_requirements(self):
-        self.build_requires("meson/0.54.2")
-        if not tools.which("pkg-config") or self.settings.os == "Windows":
-            self.build_requires("pkg-config_installer/0.29.2@bincrafters/stable")
+        self.build_requires("meson/0.53.2")
 
     def requirements(self):
         if self.options.with_pcre:
