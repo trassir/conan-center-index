77bb67351366499464fdd97d2835c3865fb0adab
a-ryabkov <80391285+a-ryabkov@users.noreply.github.com>
2021-03-12 14:17:41 +0300

    fix

--- recipes/protoc/3.9.1/conanfile.py
+++ recipes/protoc/3.9.1/conanfile.py
@@ -5,6 +5,7 @@ import os
 class ProtocConanFile(ConanFile):
     _base_name = "protobuf"
     name = "protoc"
+    version = "3.9.1"
     description = "Protocol Buffers - Google's data interchange format"
     topics = ("conan", "protobuf", "protocol-buffers", "protocol-compiler", "serialization", "rpc", "protocol-compiler")
     url = "https://github.com/bincrafters/conan-protobuf"
@@ -23,7 +24,7 @@ class ProtocConanFile(ConanFile):
     settings = "os_build", "arch_build", "compiler", "arch"
 
     def build_requirements(self):
-        self.build_requires("protobuf/3.9.1")
+        self.build_requires("protobuf/{}".format(self.version))
 
     def _configure_cmake(self):
         cmake = CMake(self)
@@ -40,8 +41,9 @@ class ProtocConanFile(ConanFile):
         cmake.build()
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
-        extracted_dir = self._base_name + "-3.9.1"
+        sha256 = "98e615d592d237f94db8bf033fba78cd404d979b0b70351a9e5aaff725398357"
+        tools.get("{0}/archive/v{1}.tar.gz".format(self.homepage, self.version), sha256=sha256)
+        extracted_dir = self._base_name + "-" + self.version
         os.rename(extracted_dir, self._source_subfolder)
 
     def imports(self):
@@ -50,6 +52,8 @@ class ProtocConanFile(ConanFile):
         if tools.os_info.is_linux:
             # `ldd` shows dependencies named like libprotoc.so.3.9.1.0
             self.protobuf_dylib_mask = "*.so.*"
+        else:
+            assert False, "protoc package was not checked on your system"
         self.copy(self.protobuf_dylib_mask, dst="lib", src="lib", root_package="protobuf")
 
     def package(self):
